2a3362786b15db4e3bf559f08f8e696e59f9513f
Ahmad Ali Asif <spyali463@gmail.com>
2023-03-26 16:57:59 +0500

    practice java

--- script.js
+++ script.js
@@ -12,3 +12,15 @@ console.log(program);
 var num1 = 2;
 var num2 = 10;
 console.log(num1*num2);
+
+
+function addnumber(a,b) {
+    var result = (a-b)
+    console.log("two number is", result)
+    
+}
+addnumber(3,15)
+
+
+
+addnumber(4,50)
