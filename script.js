a589ac59222f3cb3acf80724efab8a8be61c9722
Ahmad Ali Asif <spyali463@gmail.com>
2023-03-26 13:23:01 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function evenodd() {
+    var evenodd =+ prompt("enter the number")
+    if(evenodd % 2== 0){
+        console.log("the number is even");
+    }
+    else{
+        console.log("the number is odd");
+    }
+}
+
+evenodd()
