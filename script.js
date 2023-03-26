647182084916a2222e9756d0cef8a5a805b603de
Ahmad Ali Asif <spyali463@gmail.com>
2023-03-26 16:20:49 +0500

    practice js

--- script.js
+++ script.js
@@ -1,23 +1,20 @@
-// function evenodd() {
-//     var evenodd =+ prompt("enter the number")
-//     if(evenodd % 2== 0){
-//         console.log("the number is even");
-//     }
-//     else{
-//         console.log("the number is odd");
-//     }
-// }
+var program = 2+2;
+console.log(program);
 
-// evenodd()
+var num1 = 2;
+var num2 = 2;
+console.log(num1+num2)
 
 
+var program = 2+2;
+console.log(program);
 
-function  sum(a,b) {
-    var result = (num1+num2)
-console.log("your result is this>" ,result)
-}
-var num1 =+ prompt("enter your value1  ")
-    var num2 =+ prompt("enter the the value 2")
+var num1 = 2;
+var num2 = 2;
+console.log(num1+num2)
+var program = 2+2;
+console.log(program);
 
-sum(num1,num2)
-
+var num1 = 2;
+var num2 = 2;
+console.log(num1+num2)
