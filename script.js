4be6d332920b2a0e7a4e0d7349112a8c09bcbaa8
Ahmad Ali Asif <spyali463@gmail.com>
2023-03-26 15:28:00 +0500

    javascript

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
-function evenodd() {
-    var evenodd =+ prompt("enter the number")
-    if(evenodd % 2== 0){
-        console.log("the number is even");
-    }
-    else{
-        console.log("the number is odd");
-    }
-}
+// function evenodd() {
+//     var evenodd =+ prompt("enter the number")
+//     if(evenodd % 2== 0){
+//         console.log("the number is even");
+//     }
+//     else{
+//         console.log("the number is odd");
+//     }
+// }
 
-evenodd()
+// evenodd()
+
+
+
+function  sum(a,b) {
+    var result = (num1+num2)
+console.log("your result is this>" ,result)
+}
+var num1 =+ prompt("enter your value1  ")
+    var num2 =+ prompt("enter the the value 2")
+
+sum(num1,num2)
+
