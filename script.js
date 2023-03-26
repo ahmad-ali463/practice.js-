e608f1223d6d00693dbb2a710ab0b4f1a74f88ee
Ahmad Ali Asif <spyali463@gmail.com>
2023-03-26 16:27:07 +0500

    practice js

--- script.js
+++ script.js
@@ -10,11 +10,5 @@ var program = 2+2;
 console.log(program);
 
 var num1 = 2;
-var num2 = 2;
-console.log(num1+num2)
-var program = 2+2;
-console.log(program);
-
-var num1 = 2;
-var num2 = 2;
-console.log(num1+num2)
+var num2 = 10;
+console.log(num1*num2);
