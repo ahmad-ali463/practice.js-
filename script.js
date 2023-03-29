3d3a1eb4aaf445231cad88d2df4341f292ab8ea7
Ahmad Ali Asif <spyali463@gmail.com>
2023-03-29 20:42:56 +0500

    yiii

--- script.js
+++ script.js
@@ -46,3 +46,10 @@ console.log(program);
 var num1 = 2; 
 var num2 = 10;
 console.log(num1*num2);
+
+var program = 2+2;
+console.log(program);
+
+var num1 = 2; 
+var num2 = 10;
+console.log(num1*num2);
